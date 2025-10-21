f61589f9302f756103be25c0c0bfa5a2bbf18378
Dias <dias.atudinov@gmail.com>
2025-10-21 22:25:45 +0500

    add menu view

--- Moro Drift/Moro Drift/MDModules/MDDaily/MDDailyView.swift
+++ Moro Drift/Moro Drift/MDModules/MDDaily/MDDailyView.swift
@@ -2,16 +2,103 @@
 //  MDDailyView.swift
 //  Moro Drift
 //
-//  Created by Dias Atudinov on 21.10.2025.
 //
 
 import SwiftUI
 
 struct MDDailyView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @Environment(\.presentationMode) var presentationMode
+        @State private var isReceived = false
+        var body: some View {
+            ZStack {
+                VStack {
+                    Spacer()
+                    
+                    HStack(alignment: .bottom) {
+                        Image(.person2ImgMD)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:300)
+                            .offset(y: 30)
+                        Spacer()
+                    }.ignoresSafeArea()
+                }
+                VStack {
+                    
+                    Image(.dailyIconMD)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 80:50)
+                        .opacity(0)
+                    
+                    ZStack {
+                        
+                        Image(.dailyBgMD)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        VStack {
+                            Spacer()
+                            
+                            Button {
+                                if !isReceived {
+                                    ZZUser.shared.updateUserMoney(for: 20)
+                                }
+                                isReceived.toggle()
+                            } label: {
+                                Image(isReceived ? .collectedBtnMD : .collectBtnMD)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 88:52)
+                            }
+                            
+                        }.offset(y: 15)
+                        
+                    }.frame(height: ZZDeviceManager.shared.deviceType == .pad ? 88:240)
+                    
+                }
+                
+                VStack {
+                    ZStack {
+                        
+                        HStack {
+                            Image(.dailyIconMD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 80:50)
+                        }
+                        
+                        HStack {
+                            Button {
+                                presentationMode.wrappedValue.dismiss()
+                                
+                            } label: {
+                                Image(.backIconMD)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 100:60)
+                            }
+                            
+                            Spacer()
+                            
+                            ZZCoinBg()
+                            
+                        }.padding()
+                    }
+                    Spacer()
+                    
+                }
+            }.frame(maxWidth: .infinity)
+                .background(
+                    ZStack {
+                        Image(.appBgMD)
+                            .resizable()
+                            .ignoresSafeArea()
+                            .scaledToFill()
+                    }
+                )
+        }
     }
-}
 
 #Preview {
     MDDailyView()
--- Moro Drift/Moro Drift/MDModules/MDMenu/MDMenuView.swift
+++ Moro Drift/Moro Drift/MDModules/MDMenu/MDMenuView.swift
@@ -2,16 +2,128 @@
 //  MDMenuView.swift
 //  Moro Drift
 //
-//  Created by Dias Atudinov on 21.10.2025.
 //
 
 import SwiftUI
 
 struct MDMenuView: View {
+    @State private var showGame = false
+    @State private var showAchievement = false
+    @State private var showSettings = false
+    @State private var showCalendar = false
+    @State private var showDailyReward = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            
+            VStack(spacing: 0) {
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    ZZCoinBg()
+                    
+                    
+                }.padding(20).padding(.bottom, 5)
+                Spacer()
+            }
+            
+            HStack {
+                Image(.personImgMD)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:322)
+                
+                Spacer()
+                
+            }.ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                
+                Image(.loaderViewLogoMD)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:140)
+                    .cornerRadius(12)
+                    .padding(.top, 20)
+                Spacer()
+                HStack(spacing: 10) {
+                    VStack(spacing: 10) {
+                        
+                        Button {
+                            showGame = true
+                        } label: {
+                            Image(.playIconMD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:75)
+                        }
+                        
+                        Button {
+                            showDailyReward = true
+                        } label: {
+                            Image(.dailyIconMD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 100:75)
+                        }
+                        
+                        
+                    }
+                    
+                    VStack(spacing: 10) {
+                        
+                        Button {
+                            showAchievement = true
+                        } label: {
+                            Image(.achievementsIconMD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 140:75)
+                        }
+                        
+                        Button {
+                            showSettings = true
+                        } label: {
+                            Image(.settingsIconMD)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: ZZDeviceManager.shared.deviceType == .pad ? 100:75)
+                        }
+                    }
+                }
+                Spacer()
+            }
+            
+            
+            
+        }.frame(maxWidth: .infinity)
+            .background(
+                ZStack {
+                    Image(.appBgMD)
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .scaledToFill()
+                }
+            )
+            .fullScreenCover(isPresented: $showGame) {
+                //                    GameRootView()
+            }
+            .fullScreenCover(isPresented: $showAchievement) {
+                MDAchievementsView()
+            }
+            .fullScreenCover(isPresented: $showSettings) {
+                MDSettingsView()
+            }
+            .fullScreenCover(isPresented: $showDailyReward) {
+                MDDailyView()
+            }
     }
 }
+
 
 #Preview {
     MDMenuView()
